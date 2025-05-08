8134011fa48557bb5ec1ab677b0de713b9ebf91f
David Riddervold Marconis <david.marconis@volue.com>
2025-05-08 10:53:05 +0200

    Add NOTICE.txt with license info

---
+++ bin/generate-notice.js
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import licenseChecker from 'license-checker'
+
+licenseChecker.init({ start: '.' }, (err, ret) => {
+    if (err) {
+        console.error('An error occured while checking licenses')
+        return
+    }
+    var fd = null
+    try {
+        fd = fs.openSync('NOTICE.TXT', 'w')
+        fs.appendFileSync(
+            fd,
+            'NOTICE\n\nThis project includes third-party dependencies licensed under the following terms\n\n'
+        )
+        fs.appendFileSync(fd, 'Summary of third-party licences:\n')
+        fs.appendFileSync(fd, `${licenseChecker.asSummary(ret)}\n`)
+
+        for (const [packageName, info] of Object.entries(ret)) {
+            fs.appendFileSync(fd, `${'-'.repeat(80)}\n`)
+            fs.appendFileSync(fd, `${packageName}\n\n`)
+            if (info.licenses) {
+                fs.appendFileSync(fd, `LICENSE: ${info.licenses}\n`)
+            }
+            if (info.repository) {
+                fs.appendFileSync(fd, `REPOSITORY: ${info.repository}\n`)
+            }
+            if (info.licenseFile) {
+                fs.appendFileSync(fd, `\n${fs.readFileSync(info.licenseFile)}`)
+            }
+            fs.appendFileSync(fd, '\n')
+        }
+    } finally {
+        if (fd != null) {
+            fs.closeSync(fd)
+        }
+    }
+})
