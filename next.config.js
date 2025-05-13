c4247d6976ecb06ca84d15e29a72f3439afeefcd
David Riddervold Marconis <138445089+david-marconis@users.noreply.github.com>
2025-05-13 09:07:11 +0200

    Upgrade nextjs and auth0 (#12)
    * Update next and auth0
    
    * Update attributions

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
     i18n: {
         locales: ['default', 'nb', 'en'],
         defaultLocale: 'default',
         localeDetection: false,
     },
+    devIndicators: false,
 }
 
 module.exports = nextConfig
